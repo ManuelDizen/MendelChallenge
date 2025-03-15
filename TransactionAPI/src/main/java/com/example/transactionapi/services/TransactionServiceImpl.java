d88c5b1afca7564f8e6ddf56c68af6720ffbe469
MANUEL DIZENHAUS <mdizenhaus@itba.edu.ar>
2025-03-15 11:15:27 -0300

    Service layer

--- TransactionAPI/src/main/java/com/example/transactionapi/services/TransactionServiceImpl.java
+++ TransactionAPI/src/main/java/com/example/transactionapi/services/TransactionServiceImpl.java
@@ -1,29 +1,85 @@
 package com.example.transactionapi.services;
 
+import com.example.transactionapi.interfaces.TransactionRepository;
+import com.example.transactionapi.interfaces.TransactionService;
 import com.example.transactionapi.models.Transaction;
+import com.example.transactionapi.repositories.TransactionRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
-public class TransactionService {
+public class TransactionServiceImpl implements TransactionService {
 
-    private final Map<Long, Transaction> transactions = new HashMap<>();
-    private final Map<String, Set<Long>> types = new HashMap<>();
+    private final TransactionRepository transactionRepository;
 
+    @Autowired
+    public TransactionServiceImpl(TransactionRepositoryImpl transactionRepositoryImpl) {
+        this.transactionRepository = transactionRepositoryImpl;
+    }
+
+    public Set<Transaction> getAll(){
+        return transactionRepository.getAll();
+    }
 
     public Double getTransactionSumById(Long id) {
-        //TODO: Validate if id exists
-        return -1.0;
+        if(!transactionRepository.transactionExists(id)){
+            return null;
+        }
+
+        Stack<Long> toProcess = new Stack<>();
+        Set<Long> processed = new HashSet<>();
+        Double sum = 0.0;
+        toProcess.push(id);
+        while(!toProcess.isEmpty()){
+            Long current = toProcess.pop();
+            if(!processed.contains(current)){
+                processed.add(current);
+                Transaction transaction = transactionRepository.getById(current);
+
+                Set<Long> linked = transaction.getChildTransactions();
+                toProcess.addAll(linked);
+                sum += transaction.getAmount();
+            }
+        }
+
+        return sum;
     }
 
     public Set<Long> getTransactionsByType(String type){
-        //TODO: Validate if type is key
-        return types.get(type);
+        return transactionRepository.getByType(type);
     }
 
     public void putTransaction(Long transaction_id, Double amount, String type, Long parent_id){
+        /*
+        Recibo transaccion con ID
+        1. Si existe, actualizo los campos y chequeo si cambió el tipo
+            1.1 Si no cambio el tipo, nada que hacer
+            1.2 Si cambio el tipo, tengo que:
+                1.2.1 Sacar de set anterior
+                    1.2.1.1 Si set queda vacio, borrar la entrada
+                1.2.2 Agregar a set nuevo
+                    1.2.2.1 Si set nuevo no existe, crearlo y agregarlo
+                    1.2.2.2 Si ya existe, simplemente agregar.
 
+         2. Si no existe, creo transaccion y agrego a mapa de transacciones
+            2.1 Si el tipo no existe, creo set y agrego
+            2.2 Si ya existe, simplemente agrego
+
+        Nota: Parent id puede ser null.
+
+         */
+        if(transactionRepository.transactionExists(transaction_id)){
+            Transaction transaction = transactionRepository.getById(transaction_id);
+            if(!Objects.equals(transaction.getType(), type)){
+                transactionRepository.updateType(transaction_id, transaction.getType(), type);
+            }
+            transactionRepository.updateTransaction(transaction_id, amount, type, parent_id);
+        }
+        else{
+            transactionRepository.createTransaction(transaction_id, amount, type, parent_id);
+        }
     }
 
 
