aba8c193fd7f7f523f99813d123d3c0fa8c70a8f
MANUEL DIZENHAUS <mdizenhaus@itba.edu.ar>
2025-03-15 11:14:39 -0300

    Endpoints wired to service layer

--- TransactionAPI/src/main/java/com/example/transactionapi/controllers/TransactionController.java
+++ TransactionAPI/src/main/java/com/example/transactionapi/controllers/TransactionController.java
@@ -1,6 +1,7 @@
 package com.example.transactionapi.controllers;
 
-import com.example.transactionapi.services.TransactionService;
+import com.example.transactionapi.dtos.TransactionDTO;
+import com.example.transactionapi.services.TransactionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +12,37 @@ import java.util.Collections;
 @RequestMapping("/transactions")
 public class TransactionController {
 
-    private final TransactionService transactionService;
+    private final TransactionServiceImpl transactionServiceImpl;
 
     @Autowired
-    public TransactionController(TransactionService transactionService){
-        this.transactionService = transactionService;
+    public TransactionController(TransactionServiceImpl transactionServiceImpl){
+        this.transactionServiceImpl = transactionServiceImpl;
     }
 
     @GetMapping("/sum/{transaction_id}")
-    public ResponseEntity getTransactionSumById(@PathVariable Long transaction_id){
-        Double sum = transactionService.getTransactionSumById(transaction_id);
+    public ResponseEntity<?> getTransactionSumById(@PathVariable Long transaction_id){
+        Double sum = transactionServiceImpl.getTransactionSumById(transaction_id);
+        if(sum == null){
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(Collections.singletonMap("sum", sum));
     }
     
-    @PutMapping("/transactions/{transaction_id}")
-    public ResponseEntity createTransaction(@PathVariable Long transaction_id){
-        return ResponseEntity.ok(0);
+    @PutMapping("/{transaction_id}")
+    public ResponseEntity<?> putTransaction(@PathVariable Long transaction_id,
+                                         @RequestBody TransactionDTO transaction){
+        transactionServiceImpl.putTransaction(transaction_id, transaction.getAmount(),
+                transaction.getType(), transaction.getParentId());
+        return ResponseEntity.ok("{\"status\": \"ok\"}");
     }
 
     @GetMapping("/types/{type}")
-    public ResponseEntity getTransactionByType(@PathVariable String type){
-        return ResponseEntity.ok(0);
+    public ResponseEntity<?> getTransactionByType(@PathVariable String type){
+        return ResponseEntity.ok(transactionServiceImpl.getTransactionsByType(type));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(transactionServiceImpl.getAll());
     }
 }
